641851fec7e94fa6f8c9620d21acdbeb25883693
Alejandra Cordero <alejandra.corderogarcia21@gmail.com>
2017-11-01 16:13:52 -0430

    Primera version del generador de codigo

--- navbar/urls.py
+++ navbar/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'navbar'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^showCodigo/$', views.showCodigo, name='showCodigo')
 ]
--- navbar/views.py
+++ navbar/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotAllowed, Http404, JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
+from django.template import loader
+from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
+from django.utils.html import escape
+import json
 
 def index(request):
     return render(request, 'navbar/index.html')
+
+
+@csrf_exempt
+def showCodigo(request):
+
+	html = ''
+	if request.method == 'POST':
+		print("Entre a la vista!!!")
+		url = request.POST
+
+		listaJson = []
+
+		for i in url:
+			lista = json.loads(i)
+
+		print("La lista es:",lista)	
+		html = render_to_string('navbar/navbar.html', { 'lista': lista })
+		print(html)
+
+	context = {'html':html}
+	template = loader.get_template('navbar/navbarCodigo.html')
+	return HttpResponse(template.render(context,request))
+		
+
